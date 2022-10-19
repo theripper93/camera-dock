a2dbab072ba5b4f3fd3823fa613780faf431f089
theripper93 <tsplab@gmail.com>
2022-10-19 19:00:40 +0200

    a

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,39 @@
-Hooks.once('init', async function() {
-
+Hooks.once('init', function() {
+    game.settings.register("camera-dock", "camera-size", {
+        name: "",
+        default: 200,
+        type: Number,
+        scope: "client",
+        config: false,
+        onChange: () => {
+            document.querySelector(':root').style.setProperty('--camera-size', game.settings.get("camera-dock", "camera-size") + "px");
+        },
+      });
+      document.querySelector(':root').style.setProperty('--camera-size', game.settings.get("camera-dock", "camera-size") + "px");
 });
 
-Hooks.once('ready', async function() {
+Hooks.once("ready", () => {
+    const rtcSett = game.settings.get("core", "rtcClientSettings")
 
+    rtcSett.dockPosition = "bottom";
+
+    for(let [k,v] of Object.entries(rtcSett.users)){
+        v.popout = true;
+        rtcSett.users[k] = v;
+    }
+
+    game.settings.set("core", "rtcClientSettings", rtcSett);
+})
+
+Hooks.on("renderCameraViews", (settings) => {
+    $("#ui-bottom").prepend($("#camera-views"));
+    const sizeBTN = $(`<a class="av-control" data-action="cycle-video" data-tooltip="Cycle Size" style="display: block;" aria-describedby="tooltip">
+    <i class="fas fa-arrows-alt-h"></i>
+</a>`);
+    sizeBTN.on("click", () => {
+        const size = game.settings.get("camera-dock", "camera-size");
+        game.settings.set("camera-dock", "camera-size", Math.max(100, (size + 50) % 400));
+    });
+    $(".user-controls").find("nav").append(sizeBTN);
+    
 });
